be6d25e7b43a4ae4dceb85ab2eb55689ac094f21
ktetsuo <ktetsuo@users.noreply.github.com>
2017-05-24 23:36:38 +0900

    add show icon and all team results

--- npb.py
+++ npb.py
@@ -1,10 +1,14 @@
+import os
 import sys
 import urllib.request
 import datetime
 from bs4 import BeautifulSoup
+from vfd import VFD
+from vfdimg import vfdimg
+import time
 
 class TeamInfo:
-    def __init(self):
+    def __init__(self):
         self.team = ''
         self.score = ''
         self.name = ''
@@ -41,6 +45,44 @@ for table in tables:
         result.teams[i].team = teams[i].find('a')['data-ylk'][9:12]  # slk:icon_XXX;pos:0
         result.teams[i].name = teams[i].find('a')['title']
     results.append(result)
+vfd = VFD(17, 27)
+vfd.open('/dev/ttyAMA0')
+vfd.fontxy(2, 2)
+
 for result in results:
-    print(result.teams[0].name + ' ' + result.teams[0].score
-          + ' - ' + result.teams[1].score + ' ' + result.teams[1].name)
+    t1 = result.teams[0]
+    t2 = result.teams[1]
+    print(t1.name + ' ' + t1.team + ' ' + t1.score
+          + ' - ' + t2.score + ' ' + t2.team + ' ' + t2.name)
+    dir = os.path.dirname(__file__)
+    if dir != '':
+        dir = dir + '/'
+    dir = dir + 'icon/'
+    icon1 = vfdimg(dir + t1.team + '.bmp')
+    icon2 = vfdimg(dir + t2.team + '.bmp')
+    x = 280
+    vfd.cursor(x, 0)
+    vfd.drawimg(icon1)
+    x += 16 + 4
+    vfd.cursor(x, 0)
+    s = t1.score + ' - ' + t2.score
+    vfd.puts(s)
+    x += len(s) * 7 * 2 + 4
+    vfd.cursor(x, 0)
+    vfd.drawimg(icon2)
+    x += 16 + 20
+    vfd.cursor(x, 0)
+    vfd.scroll(2, 512, 1)
+    vfd.clr()
+time.sleep(5)
+vfd.fontxy(1, 1)
+pos = [(0,0), (95,0), (190,0), (0,1), (95, 1), (190,1)]
+i = 0
+for result in results:
+    vfd.cursor(pos[i][0], pos[i][1])
+    t1 = result.teams[0]
+    t2 = result.teams[1]
+    s = t1.team + ' ' + t1.score + '-' + t2.score + ' ' + t2.team
+    vfd.puts(s)
+    i += 1
+vfd.close()
